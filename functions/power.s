a0a8eee4fbe8d705e059c64024abe739797374be
IDragnev <iddragnev@gmail.com>
2020-10-29 22:05:51 +0200

    Make the power function accept 0 as a power

--- functions/power.s
+++ functions/power.s
@@ -1,5 +1,5 @@
 # A simple program which calls a `power`
-# function (using the C calling convention)
+# function using the C calling convention
 
 .section .data
 
@@ -9,7 +9,7 @@
 #
 # Function: pow(base, power) -> base ^ power
 # PRECONDITIONS:
-#  1. power must be >= 1
+#  1. power must be >= 0
 #
 # Register variables:
 #  %rbx - base
@@ -21,13 +21,15 @@
 #
 .type power, @function
 power:
-pushq %rbp              # save callee BP
-movq %rsp, %rbp         # BP --> [callee BP]
+pushq %rbp              # save caller BP
+movq %rsp, %rbp         # BP --> [caller BP]
 subq $8, %rsp           # allocate space for acc on the stack
 
-movq 16(%rbp), %rbx     # %rbx = base
 movq 24(%rbp), %rcx     # %rcx = power
+cmpq $0, %rcx           # if power == 0, return 1
+je early_return
 
+movq 16(%rbp), %rbx     # %rbx = base
 movq %rbx, -8(%rbp)     # acc = base
 
 power_loop_start:
@@ -41,14 +43,17 @@ movq %rax, -8(%rbp)     # acc = temp
 decq %rcx               # power -= 1
 jmp power_loop_start
 
+early_return:
+movq $1, -8(%rbp)       # acc = 1
+
 end_power:
 movq -8(%rbp), %rax     # result = acc
-movq %rbp, %rsp         # remove local variables from the stack; SP --> [callee BP]
+movq %rbp, %rsp         # remove local variables from the stack; SP --> [caller BP]
 popq %rbp               # retore BP
 ret
 
 _start:
-pushq $3              # arg 2
+pushq $0              # arg 2
 pushq $2              # arg 1
 call power
 addq $16, %rsp        # remove args from the stack
