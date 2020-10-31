67e747aa1c1d05c898593dd5e80331d1c360f096
IDragnev <iddragnev@gmail.com>
2020-10-31 23:13:54 +0200

    Fix indentation of the source in basics/

--- basics/exit.s
+++ basics/exit.s
@@ -6,7 +6,7 @@
 .section .text
 .globl _start
 _start:
-movq $1, %rax    # the exit syscall number
-movq $0, %rbx    # the exit code of the program
+    movq $1, %rax    # the exit syscall number
+    movq $0, %rbx    # the exit code of the program
 
-int $0x80        # generate a software interrupt
+    int $0x80        # generate a software interrupt
--- basics/max.s
+++ basics/max.s
@@ -22,22 +22,22 @@ array:
 
 .globl _start
 _start:
-movq $0, %rdi                      # index = 0
-movq array(,%rdi,8), %rax          # current = array[8*0]
-movq %rax, %rbx                    # max = current
+    movq $0, %rdi                      # index = 0
+    movq array(,%rdi,8), %rax          # current = array[8*0]
+    movq %rax, %rbx                    # max = current
 
 loop_start:
-cmpq array_size, %rdi              # break if index >= size
-jge loop_exit
+    cmpq array_size, %rdi              # break if index >= size
+    jge loop_exit
 
-movq array(,%rdi, 8), %rax         # current = array[8 * index]
-incq %rdi                          # index += 1
-cmpq %rbx, %rax                    # if current <= max proceed with next item
-jle loop_start
+    movq array(,%rdi, 8), %rax         # current = array[8 * index]
+    incq %rdi                          # index += 1
+    cmpq %rbx, %rax                    # if current <= max proceed with next item
+    jle loop_start
 
-movq %rax, %rbx                    # max = current
-jmp loop_start                     # proceed with next item
+    movq %rax, %rbx                    # max = current
+    jmp loop_start                     # proceed with next item
 
 loop_exit:
-movq $1, %rax                      # the exit syscall number
-int $0x80                          # generate software interrupt
+    movq $1, %rax                      # the exit syscall number
+    int $0x80                          # generate software interrupt
