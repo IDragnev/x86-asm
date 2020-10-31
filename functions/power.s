21f13ee9455a3b8476fe32ad32e5e3baa3a48a71
IDragnev <iddragnev@gmail.com>
2020-10-31 22:52:45 +0200

    Remove the calling code from power.s

--- functions/power.s
+++ functions/power.s
@@ -1,13 +1,14 @@
-# A simple program which calls a `power`
-# function using the C calling convention
-
 .section .data
 
 .section .text
-.globl _start
+.globl power
 
 #
-# Function: pow(base, power) -> base ^ power
+# Function:
+# pow(base, power) -> base ^ power
+#
+# Uses the C calling convention
+#
 # PRECONDITIONS:
 #  1. power must be >= 0
 #
@@ -21,50 +22,33 @@
 #
 .type power, @function
 power:
-pushq %rbp              # save caller BP
-movq %rsp, %rbp         # BP --> [caller BP]
-subq $8, %rsp           # allocate space for acc on the stack
+    pushq %rbp              # save caller BP
+    movq %rsp, %rbp         # BP --> [caller BP]
+    subq $8, %rsp           # allocate space for acc on the stack
 
-movq 24(%rbp), %rcx     # %rcx = power
-cmpq $0, %rcx           # if power == 0, return 1
-je early_return
+    movq 24(%rbp), %rcx     # %rcx = power
+    cmpq $0, %rcx           # if power == 0, return 1
+    je early_return
 
-movq 16(%rbp), %rbx     # %rbx = base
-movq %rbx, -8(%rbp)     # acc = base
+    movq 16(%rbp), %rbx     # %rbx = base
+    movq %rbx, -8(%rbp)     # acc = base
 
 power_loop_start:
-cmpq $1, %rcx           # if power == 1, we are done
-je end_power
+    cmpq $1, %rcx           # if power == 1, we are done
+    je end_power
 
-movq -8(%rbp), %rax     # temp = acc
-imulq %rbx, %rax        # temp *= base
-movq %rax, -8(%rbp)     # acc = temp
+    movq -8(%rbp), %rax     # temp = acc
+    imulq %rbx, %rax        # temp *= base
+    movq %rax, -8(%rbp)     # acc = temp
 
-decq %rcx               # power -= 1
-jmp power_loop_start
+    decq %rcx               # power -= 1
+    jmp power_loop_start
 
 early_return:
-movq $1, -8(%rbp)       # acc = 1
+    movq $1, -8(%rbp)       # acc = 1
 
 end_power:
-movq -8(%rbp), %rax     # result = acc
-movq %rbp, %rsp         # remove local variables from the stack; SP --> [caller BP]
-popq %rbp               # retore BP
-ret
-
-_start:
-pushq $0              # arg 2
-pushq $2              # arg 1
-call power
-addq $16, %rsp        # remove args from the stack
-pushq %rax            # save the return value on the stack
-
-pushq $2              # arg 2
-pushq $5              # arg 1
-call power
-addq $16, %rsp        # remove args from the stack, SP --> [ret1]
-popq %rbx             # %rbx = ret1
-addq %rax, %rbx       # exit code = ret1 + ret2
-
-movq $1, %rax         # the exit syscall number
-int $0x80             # generate a software interrupt
+    movq -8(%rbp), %rax     # result = acc
+    movq %rbp, %rsp         # remove local variables from the stack; SP --> [caller BP]
+    popq %rbp               # retore BP
+    ret
